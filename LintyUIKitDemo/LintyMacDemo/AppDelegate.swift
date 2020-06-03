12ba850f8a4b8b1baf85eba6e530779ecdeca6d1
qmchenry <quinn@jqm.us>
2020-06-03 15:06:13 -0400

    Create macOS test application

---
+++ LintyUIKitDemo/LintyMacDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LintyMacDemo
+//
+//  Created by Quinn McHenry on 6/1/20.
+//  Copyright © 2020 Quinn McHenry. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LintyUIKitDemo/LintyMacDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LintyMacDemo
+//
+//  Created by Quinn McHenry on 6/1/20.
+//  Copyright © 2020 Quinn McHenry. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

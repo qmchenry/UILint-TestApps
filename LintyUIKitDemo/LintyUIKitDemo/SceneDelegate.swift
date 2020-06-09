c515e36a8e3296a09dac08de356147a23501c99b
qmchenry <quinn@jqm.us>
2020-06-09 16:40:24 -0400

    Update test app to recent UILint changes
    Also removed Mac target after abandoning this path for UILint.

--- LintyUIKitDemo/LintyUIKitDemo/SceneDelegate.swift
+++ LintyUIKitDemo/LintyUIKitDemo/SceneDelegate.swift
@@ -19,6 +19,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
         UILint.register(window: window)
+
+        QAConfig.shared.expectedFontNames = [".SFUI-Regular", "Futura-Medium", "Futura-Bold", "Futura-MediumItalic" ]
+//        QAConfig.shared.excludedChecks = [LabelUnexpectedFont.self]
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -28,28 +31,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
         UILint.deregister()
     }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-    }
-
 
 }
 
--- LintyUIKitDemo/LintyUIKitDemo/ViewController.swift
+++ LintyUIKitDemo/LintyUIKitDemo/ViewController.swift
@@ -19,6 +19,4 @@ class ViewController: UIViewController {
         }
     }
 
-
 }
-

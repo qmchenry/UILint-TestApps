5d27e9a85b576f58cd8ab93645f88deb845a2c1c
Quinn McHenry <quinn@jqm.us>
2020-07-09 16:09:20 -0400

    Enhance UI testing integration

--- LintyUIKitDemo/LintyUIKitDemoUITests/LintyUIKitDemoUITests.swift
+++ LintyUIKitDemo/LintyUIKitDemoUITests/LintyUIKitDemoUITests.swift
@@ -10,29 +10,12 @@ import XCTest
 import UILint
 
 class LintyUIKitDemoUITests: XCTestCase {
-
-    func performUILint(app: XCUIApplication, attachReport: Bool = false) {
-        let element = app.windows.children(matching: .other).element.children(matching: .other)
-            .element.children(matching: .other).element
-        element.tap(withNumberOfTaps: 2, numberOfTouches: 2)
-
-        let exists = NSPredicate(format: "exists == 1")
-        expectation(for: exists, evaluatedWith: app.navigationBars["UIActivityContentView"], handler: nil)
-        waitForExpectations(timeout: 50, handler: nil)
-        app.navigationBars["UIActivityContentView"].buttons["Close"].tap()
-
-        if attachReport {
-            let url = URL(fileURLWithPath: "/tmp/uilint-report.pdf")
-            let attachment = XCTAttachment(contentsOfFile: url)
-            attachment.lifetime = .keepAlways
-            add(attachment)
-        }
-    }
     
     func testUILintExample() throws {
         let app = XCUIApplication()
         app.launch()
         performUILint(app: app, attachReport: true)
+        XCTAssertUILintClean()
     }
 
 }
